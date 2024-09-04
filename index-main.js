8b45571aaa1d8e51adf3e8a2158e82844c283a75
SerhiiSydorchuk <sydorchuks93@gmail.com>
2024-09-04 16:48:55 +0200

    first commit

---
+++ index-main.js
@@ -0,0 +1,19 @@
+let usersDiv = document.getElementById('users-wrap');
+fetch(`https://jsonplaceholder.typicode.com/users`)
+    .then(res => res.json())
+    .then(users => {
+        for (const user of users) {
+            const userDiv = document.createElement('div');
+            const h2 = document.createElement('h2')
+            const btn = document.createElement('button');
+            btn.onclick = function () {
+                window.location.href = `user-details.html?id=${user.id}`
+            };
+            userDiv.append(h2 , btn);
+            btn.classList.add('btn-user-info');
+            btn.innerText = `Information about user ${user.id}`;
+            usersDiv.appendChild(userDiv);
+            userDiv.classList.add('user');
+            h2.innerText = `${user.id} ${user.name}`
+        }
+    });
