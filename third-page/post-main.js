90c35946d0c24f6a3760ad487bd4dfad103b7d8a
SerhiiSydorchuk <sydorchuks93@gmail.com>
2024-09-09 10:48:48 +0200

    first commit

--- third-page/post-main.js
+++ third-page/post-main.js
@@ -6,11 +6,11 @@ const toUpFirstLetter = (msg)=>{
         msg;
 }
 
-const deleteSlash = msg => msg.replaceAll(/\n/g, ' ');
+
 //Reusable function to creat paragraph
 const creatParagraph = (key, value,div) => {
     const p = document.createElement('p');
-    p.innerText = `${toUpFirstLetter(key)}: ${toUpFirstLetter(deleteSlash(value))})`
+    p.innerText = `${toUpFirstLetter(key)}: ${toUpFirstLetter(value)}`
     return p;
 };
 //Reusable function to creat div with class
@@ -19,45 +19,34 @@ const creatDivWithClass = (divName , className) =>{
     divName.classList.add(className);
     return divName;
 }
-console.log();
-
-
-
 
 async function addUserPost (idPost){
     try {
         const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?id=${idPost}`).then(response => response.json());
         for (const post of posts) {
+            const divPost = creatDivWithClass('divPost', 'post-wrap');
 
-            const divPost = creatDivWithClass('divPost', 'post-wrap')
-            const divComment = creatDivWithClass('divComment', 'comments-wrap')
             for (const postKey in post) {
-                if (postKey ==='body'){
-                  let p =  creatParagraph(postKey, post[postKey],divPost);
-                   divPost.appendChild(p);}else {
-                const p = document.createElement('p')
-                p.innerText = `${toUpFirstLetter(postKey)}: ${toUpFirstLetter(post[postKey])}`
-                divPost.appendChild(p)}
+                const p = creatParagraph(postKey,post[postKey])
+                divPost.appendChild(p);
             }
-            postsDiv.append(divPost, divComment);
-        const comments = await fetch(`https://jsonplaceholder.typicode.com/posts/${post.id}/comments`).then(response => response.json());
-        for (const comment of comments) {
-            const divInfo = creatDivWithClass('divInfo', 'comment-info');
-            divComment.appendChild(divInfo);
-            for (const commentKey in comment) {
-                if (commentKey ==='body'){
-                    const p = creatParagraph(commentKey, comment[commentKey]);
-                    divInfo.appendChild(p);}
-                else {
-                    const p = document.createElement('p')
-                    p.innerText = `${toUpFirstLetter(commentKey)}: ${toUpFirstLetter(comment[commentKey])}`
+            const comments = await fetch(`https://jsonplaceholder.typicode.com/posts/${post.id}/comments`).then(response => response.json());
+            const divComment = creatDivWithClass('divComment', 'comments-wrap');
+            for (const comment of comments) {
+                const divInfo = creatDivWithClass('divInfo', 'comment-info');
+                for (const commentKey in comment) {
+                    const p = creatParagraph(commentKey, comment[commentKey])
                     divInfo.appendChild(p);
                 }
+                divComment.appendChild(divInfo);
             }
+            postsDiv.append(divPost, divComment);
         }
-        }
+
     } catch (error) {
+
         console.error("Error fetching user info or posts:", error);
+
 }
 }
 
